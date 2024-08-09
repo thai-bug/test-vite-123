31af054b842e48d3a03562088896c82dd59379c5
null <null>
2024-08-09 22:07:22 +0700

    /sigh

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,32 @@
-import React, { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import { routeTree } from "./routeTree.gen";
+import { RecoilRoot } from "recoil";
+import { ContextProvider as ArcoContextProvider } from "@arco-design/mobile-react";
+import enUS from "@arco-design/mobile-utils/esm/locale/en-US";
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
 // Render the app
-const rootElement = document.getElementById('root')!
+const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
+  const root = ReactDOM.createRoot(rootElement);
   root.render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
-  )
+    <RecoilRoot>
+      <ArcoContextProvider locale={enUS}>
+        <RouterProvider router={router} />
+      </ArcoContextProvider>
+    </RecoilRoot>
+  );
 }
