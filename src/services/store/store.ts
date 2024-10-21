a5d85ceb3054ee79c6efbc05032affc6b1ef85cd
thai-bug <null>
2024-10-21 10:28:12 +0700

    There's four sides to every story.

--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -1,11 +1,11 @@
 import httpClient from "@/client/httpClient";
 
 export const getAllStores = async () => {
-  const response = await httpClient.get("/api/v1/stores")
-  return response.data
-}
+  const response = await httpClient.get("/api/v1/stores");
+  return response.data;
+};
 
 export const getDetailStore = async (id: string) => {
-  const response = await httpClient.get(`/api/v1/stores/${id}`)
-  return response.data
-}
+  const response = await httpClient.get(`/api/v1/stores/${id}`);
+  return response.data;
+};
