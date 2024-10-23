ff19cd9c9b3c0cfe7582292bcf6e1a23ae7e497f
thai-bug <hxt12021999@gmail.com>
2024-10-23 20:09:48 +0700

    Best commit ever

--- src/routes/fulfillment/inbound/storage-labels/index.tsx
+++ src/routes/fulfillment/inbound/storage-labels/index.tsx
@@ -1,7 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
-// import StorageLabels from "@/pages/fulfillments/inbound/storage-labels";
+import StorageLabels from "@/pages/fulfillment/inbound/storage-labels";
 import MainLayout from "@/components/layouts/MainLayout";
 
 export const Route = createFileRoute("/fulfillment/inbound/storage-labels/")({
-  component: () => <MainLayout>{/* <StorageLabels /> */}</MainLayout>,
+  component: () => (
+    <MainLayout>
+      <StorageLabels />
+    </MainLayout>
+  ),
 });
